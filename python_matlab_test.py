6c687c79622964c65ded83708513151f8fcb0c14
cgrooves <caleb.j.groves@gmail.com>
2018-04-09 23:44:17 -0600

    Fixed format so Python/Matlab communication happens

--- python_matlab_test.py
+++ python_matlab_test.py
@@ -3,4 +3,5 @@ import matlab.engine
 eng = matlab.engine.start_matlab()
 
 # call the mav_response function
-out = eng.mav_response([0.88,.041],[.88,.16,.304],[.0181,.0039],[-1.6,-.4338,0],[.234,.52],0)
+out = eng.mav_response(matlab.double([0.88,.041]),matlab.double([.88,.16,.304]),
+matlab.double([.0181,.0039]),matlab.double([-1.6,-.4338,0]),matlab.double([.234,.52]),0)
