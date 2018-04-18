1b8fe44bc6a9beb58bd2b4edabbc4bbd8232859e
Justin Roberts <justinrobertsdw@gmail.com>
2018-04-17 22:12:08 -0600

    Ran a few doe's with wider ranges, made doe for large range and large num. of runs

--- run_doe.m
+++ run_doe.m
@@ -1,4 +1,4 @@
-filein_path = 'Inputs/Space Filling Latin Hypercube 50 inputs.csv';
-fileout_path = 'Outputs/sfud_50_003.csv';
+filein_path = 'Inputs/Space Filling Latin Hypercube 5000 inputs wide range.csv';
+fileout_path = 'Outputs/sflhc_5000_001.csv';
 
 supermav_doe(filein_path,fileout_path)
